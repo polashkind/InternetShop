1d4b3b6e076bc644d98aad9501753cbfc8072ed1
Polina Marinovich <polashkind@gmail.com>
2023-04-05 10:45:40 +0300

    implementation business logic layer

---
+++ InternetShop/BLL/DI/BusinessLogicRegister.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using BLL.Services;
+using BLL.Interfaces;
+
+namespace BLL.DI
+{
+    public static class BusinessLogicRegister
+    {
+        public static void AddBusinessLogic(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
