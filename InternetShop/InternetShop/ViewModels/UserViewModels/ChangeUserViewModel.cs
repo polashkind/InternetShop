b543eea9901ac2367c56eba0d09aac58987cece8
Polina Marinovich <polashkind@gmail.com>
2023-04-06 14:18:41 +0300

    changed returned type in controller methods on use UserViewModel

---
+++ InternetShop/InternetShop/ViewModels/UserViewModels/ChangeUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternetShop.ViewModels.UserViewModels
+{
+	public class ChangeUserViewModel
+	{
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
+
