4ee3c391a632fde8f251b6f3f58b422f1e8809da
Polina Marinovich <polashkind@gmail.com>
2023-04-10 14:43:37 +0300

    remove extra file

--- InternetShop/InternetShop/Controllers/UserController.cs
+++ InternetShop/InternetShop/Controllers/UserController.cs
@@ -27,7 +27,7 @@ namespace InternetShop.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<UserViewModel?> GetById([FromQuery] int id, CancellationToken cancellationToken)
+        public async Task<UserViewModel?> GetById(int id, CancellationToken cancellationToken)
         {
             var user = await _userService.GetById(id, cancellationToken);
             var mappedUser = _mapper.Map<UserModel>(user);
