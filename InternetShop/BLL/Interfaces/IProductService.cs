ea4714edc3233450decadbe67b0c58d12dac4555
Polina Marinovich <polashkind@gmail.com>
2023-04-11 23:27:31 +0300

    implement bll layer for product

---
+++ InternetShop/BLL/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using BLL.Models;
+
+namespace BLL.Interfaces
+{
+	public interface IProductService
+	{
+        Task<IEnumerable<ProductModel>> GetAll(CancellationToken cancellationToken);
+        Task<ProductModel?> GetById(int id, CancellationToken cancellationToken);
+        Task Delete(int id, CancellationToken cancellationToken);
+        Task<ProductModel?> Update(ProductModel productModel, CancellationToken cancellationToken);
+    }
+}
+
---
+++ InternetShop/BLL/Services/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.Models;
+using DAL.Entities;
+using DAL.Interfaces;
+using DAL.Repositories;
+
+namespace BLL.Services
+{
+	public class ProductService : IProductService
+	{
+        protected readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAll(CancellationToken cancellationToken)
+        {
+            var users = _mapper.Map<IEnumerable<ProductModel>>(await _productRepository.GetAll(cancellationToken));
+            return users;
+        }
+
+        public async Task<ProductModel?> GetById(int id, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetById(id, cancellationToken);
+            var mappedProduct = _mapper.Map<ProductModel>(product);
+            return mappedProduct;
+        }
+
+        public async Task<ProductModel?> Create(ProductModel productModel, CancellationToken cancellationToken)
+        {
+            var mappedProduct = _mapper.Map<ProductEntity>(productModel);
+            var result = await _productRepository.Create(mappedProduct, cancellationToken);
+            return _mapper.Map<ProductModel>(result);
+        }
+
+        public async Task Delete(int id, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetById(id, cancellationToken);
+            await _productRepository.Delete(product, cancellationToken);
+        }
+
+        public async Task<ProductModel?> Update(ProductModel productModel, CancellationToken cancellationToken)
+        {
+            var mappedProduct = _mapper.Map<ProductEntity>(productModel);
+            var result = await _productRepository.Update(mappedProduct, cancellationToken);
+            return _mapper.Map<ProductModel>(result);
+        }
+    }
+}
+
