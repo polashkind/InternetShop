ccb4205fb3f5acbd50411ef99b1ccfd0b5121edc
Polina Marinovich <polashkind@gmail.com>
2023-04-19 16:28:39 +0300

    add crud operation for basket on dal

--- InternetShop/DAL/DI/DataAccessRegister.cs
+++ InternetShop/DAL/DI/DataAccessRegister.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using DAL.Repositories;
 using DAL.Interfaces;
-using DAL.Entities;
 
 namespace DAL.DI
 {
@@ -14,8 +13,9 @@ namespace DAL.DI
 		{
 			services.AddScoped<IUserRepository, UserRepository>();
 			services.AddScoped<IProductRepository, ProductRepository>();
-			// services.AddScoped(typeof(IGenericRepository<UserEntity>), typeof(GenericRepository<UserEntity>));
-			services.AddDbContext<DatabaseContext>(context =>
+            // services.AddScoped(typeof(IGenericRepository<UserEntity>), typeof(GenericRepository<UserEntity>));
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddDbContext<DatabaseContext>(context =>
 			{
 				context.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
 			});
