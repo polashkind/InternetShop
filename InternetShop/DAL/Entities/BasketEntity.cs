8f9eb78399824401a3410efea3ebb93e0dd33f28
Polina Marinovich <polashkind@gmail.com>
2023-04-19 13:59:16 +0300

    add migration for basket table

---
+++ InternetShop/DAL/Entities/BasketEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities
+{
+	public class BasketEntity
+	{
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public decimal To_pay { get; set; }
+    }
+}
+
