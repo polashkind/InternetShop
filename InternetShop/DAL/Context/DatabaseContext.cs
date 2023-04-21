9adb8b9e58d11d55e7f6f34dffbd6c3fa644d3d1
Polina Marinovich <polashkind@gmail.com>
2023-04-21 13:13:42 +0300

    add migration for relationship between basket and product

--- InternetShop/DAL/Context/DatabaseContext.cs
+++ InternetShop/DAL/Context/DatabaseContext.cs
@@ -13,11 +13,24 @@ namespace DAL.Context
 		public DbSet<UserEntity> Users { get; set; }
 		public DbSet<ProductEntity> Products { get; set; }
         public DbSet<BasketEntity> Baskets { get; set; }
+        public DbSet<BasketItemEntity> BasketItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 			modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+
+			modelBuilder.Entity<BasketItemEntity>()
+				.HasKey(bi => new { bi.BasketId, bi.ProductId });
+			modelBuilder.Entity<BasketItemEntity>()
+				.HasOne(bi => bi.Product)
+				.WithMany(b => b.BasketItems)
+				.HasForeignKey(bi => bi.ProductId);
+            modelBuilder.Entity<BasketItemEntity>()
+                .HasOne(bi => bi.Basket)
+                .WithMany(b => b.BasketItems)
+                .HasForeignKey(bi => bi.BasketId);
+
         }
     }
 }
